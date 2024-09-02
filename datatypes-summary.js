db5e5e684687d4754421c4d37c4a9e5658c84579
Prashant062 <prashantrawat786216@gmail.com>
2024-09-02 22:38:28 +0530

    datatypes

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -8,8 +8,8 @@ const scoreValue = 100.3
 const isLoggedIn = false
 const outsideTemp = null
 let userEmail;
-
-// const id = Symbol('123')
+ 
+// const id = Symbol('123')  
 // const anotherId = Symbol('123')
 
 // console.log(id === anotherId);
